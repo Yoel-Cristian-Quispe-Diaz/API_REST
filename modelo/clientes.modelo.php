14628b3e423fd2048f3855c172fc936bbb125689
Yoel-Cristian-Quispe-Diaz <e117303@uajms.edu.bo>
2024-10-03 11:17:28 -0400

    Commit: Arreglo de error

--- modelo/clientes.modelo.php
+++ modelo/clientes.modelo.php
@@ -3,7 +3,7 @@ class ModeloCliente{
     static public function index($tabla){
         $stmt=Conexion::conectar()->prepare("select * from $tabla");
         $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_CLASS);
+        return $stmt->fetchAll();
         $stmt->close();
         $stmt->null;
     }
